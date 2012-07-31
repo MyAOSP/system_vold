CyanogenMod/android_system_vold

Allow mounting of multiple volumes via mass storage (vold part)
commit 662006fe5419803938790a164cb1ba73b05e3f41
Author: Pawit Pornkitprasan <p.pawit@gmail.com>
Date:   Sat Dec 3 13:31:46 2011 +0700

    Extracted primary storage detection into isPrimaryStorage()

    Change-Id: Ia76ace183869283e7e6563ea8d5eb919283ffe55

commit 0303f8b0722bfac3c1b0a522f42c921e3ed4666a
Author: Genokolar <genokolar@gmail.com>
Date:   Sat Oct 1 21:34:00 2011 +0800

    Add custom second lun number support

    like u8800 have different second lun number
    use TARGET_USE_CUSTOM_SECOND_LUN_NUM options to custom lun number

    Change-Id: I36f8c92f1f8041efeaa0304795a11eb1cc49e5e1
    (cherry picked from commit 24860389177df3aa839a8eb77d79608b15e0af8d)

commit 4b99cd585d12fa47b2f8d99cde430f37d2fe7293
Author: Alex Zepeda <alex@inferiorhumanorgans.com>
Date:   Mon Jul 18 10:55:33 2011 -0700

    Use a list of possible lun paths instead of a hardcoded one.

    This will easily play nice with kernels that use gadget drivers and
    function drivers (ex: tethering support doesn't require a new vold).

    Change-Id: I71796114501cf2d64dea5fd077ac5baf54873af4

commit 1f822e51cde73bc0d8e1b575848614f696534a88
Author: mik_os <kiril.mik.os@gmail.com>
Date:   Mon Feb 14 16:10:36 2011 +0200

    Added ability to use custom lun path.

    Change-Id: If39e445383c0ea0d64556df5708de80c06056174
    (cherry picked from commit 5edf3ce983ddd3ffed01c398244493ab962e3304)

commit baa2d75929be1bebb1e04d93044d7718460d5b14
Author: Josh Stone <cuviper@gmail.com>
Date:   Sat Jan 22 11:52:01 2011 -0800

    vold: cleanupAsec should only do work for EXTERNAL_STORAGE

    For devices like DINC which have both emmc and sdcard storage, only one
    of these is used for ASEC mounts.  When we're unmounting volumes, we
    need to discriminate which is which before forcing ASEC closures.

    Change-Id: I94efda36414bb0eb2c4c4e31891ce4f318582a8e
    (cherry picked from commit 61272bf32aff5515aa6f48acb2e8ea7e95b47aa3)

commit

Volume.cpp
@@ -166,6 +166,11 @@ int Volume::handleBlockEvent(NetlinkEvent *evt) {
     return -1;
 }
 
+bool Volume::isPrimaryStorage() {
+    const char* externalStorage = getenv("EXTERNAL_STORAGE") ? : "/mnt/sdcard";
+    return !strcmp(getMountpoint(), externalStorage);
+}
+
 void Volume::setState(int state) {
     char msg[255];
     int oldState = mState;
@@ -298,8 +303,7 @@ int Volume::mountVol() {
     dev_t deviceNodes[4];
     int n, i, rc = 0;
     char errmsg[255];
-    const char* externalStorage = getenv("EXTERNAL_STORAGE");
-    bool primaryStorage = externalStorage && !strcmp(getMountpoint(), externalStorage);
+    bool primaryStorage = isPrimaryStorage();
     char decrypt_state[PROPERTY_VALUE_MAX];
     char crypto_state[PROPERTY_VALUE_MAX];
     char encrypt_progress[PROPERTY_VALUE_MAX];
@@ -598,6 +602,7 @@ int Volume::doUnmount(const char *path, bool force) {
 
 int Volume::unmountVol(bool force, bool revert) {
     int i, rc;
+    const char* externalStorage = getenv("EXTERNAL_STORAGE");
 
     if (getState() != Volume::State_Mounted) {
         SLOGE("Volume %s unmount request when not mounted", getLabel());
@@ -620,24 +625,27 @@ int Volume::unmountVol(bool force, bool revert) {
 
     protectFromAutorunStupidity();
 
-    /*
-     * Unmount the tmpfs which was obscuring the asec image directory
-     * from non root users
-     */
+    /* Undo createBindMounts(), which is only called for primary storage */
+    if (isPrimaryStorage()) {
+        /*
+         * Unmount the tmpfs which was obscuring the asec image directory
+         * from non root users
+         */
 
-    if (doUnmount(Volume::SEC_STG_SECIMGDIR, force)) {
-        SLOGE("Failed to unmount tmpfs on %s (%s)", SEC_STG_SECIMGDIR, strerror(errno));
-        goto fail_republish;
-    }
+        if (doUnmount(Volume::SEC_STG_SECIMGDIR, force)) {
+            SLOGE("Failed to unmount tmpfs on %s (%s)", SEC_STG_SECIMGDIR, strerror(errno));
+            goto fail_republish;
+        }
 
-    /*
-     * Remove the bindmount we were using to keep a reference to
-     * the previously obscured directory.
-     */
+        /*
+         * Remove the bindmount we were using to keep a reference to
+         * the previously obscured directory.
+         */
 
-    if (doUnmount(Volume::SEC_ASECDIR, force)) {
-        SLOGE("Failed to remove bindmount on %s (%s)", SEC_ASECDIR, strerror(errno));
-        goto fail_remount_tmpfs;
+        if (doUnmount(Volume::SEC_ASECDIR, force)) {
+            SLOGE("Failed to remove bindmount on %s (%s)", SEC_ASECDIR, strerror(errno));
+            goto fail_remount_tmpfs;
+        }
     }
 
     /*

MyAOSP/system_vold

Volume.cpp
@@ -170,6 +170,11 @@ int Volume::handleBlockEvent(NetlinkEvent *evt) {
     return -1;
 }
 
+bool Volume::isPrimaryStorage() {
+    const char* externalStorage = getenv("EXTERNAL_STORAGE") ? : "/mnt/sdcard";
+    return !strcmp(getMountpoint(), externalStorage);
+}
+
 void Volume::setState(int state) {
     char msg[255];
     int oldState = mState;
@@ -302,8 +307,7 @@ int Volume::mountVol() {
     dev_t deviceNodes[4];
     int n, i, rc = 0;
     char errmsg[255];
-    const char* externalStorage = getenv("EXTERNAL_STORAGE");
-    bool primaryStorage = externalStorage && !strcmp(getMountpoint(), externalStorage);
+    bool primaryStorage = isPrimaryStorage();
     char decrypt_state[PROPERTY_VALUE_MAX];
     char crypto_state[PROPERTY_VALUE_MAX];
     char encrypt_progress[PROPERTY_VALUE_MAX];
@@ -602,6 +606,7 @@ int Volume::doUnmount(const char *path, bool force) {
 
 int Volume::unmountVol(bool force, bool revert) {
     int i, rc;
+    const char* externalStorage = getenv("EXTERNAL_STORAGE");
 
     if (getState() != Volume::State_Mounted) {
         SLOGE("Volume %s unmount request when not mounted", getLabel());
@@ -624,24 +629,27 @@ int Volume::unmountVol(bool force, bool revert) {
 
     protectFromAutorunStupidity();
 
-    /*
-     * Unmount the tmpfs which was obscuring the asec image directory
-     * from non root users
-     */
+    /* Undo createBindMounts(), which is only called for primary storage */
+    if (isPrimaryStorage()) {
+        /*
+         * Unmount the tmpfs which was obscuring the asec image directory
+         * from non root users
+         */
 
-    if (doUnmount(Volume::SEC_STG_SECIMGDIR, force)) {
-        SLOGE("Failed to unmount tmpfs on %s (%s)", SEC_STG_SECIMGDIR, strerror(errno));
-        goto fail_republish;
-    }
+        if (doUnmount(Volume::SEC_STG_SECIMGDIR, force)) {
+            SLOGE("Failed to unmount tmpfs on %s (%s)", SEC_STG_SECIMGDIR, strerror(errno));
+            goto fail_republish;
+        }
 
-    /*
-     * Remove the bindmount we were using to keep a reference to
-     * the previously obscured directory.
-     */
+        /*
+         * Remove the bindmount we were using to keep a reference to
+         * the previously obscured directory.
+         */
 
-    if (doUnmount(Volume::SEC_ASECDIR_EXT, force)) {
-        SLOGE("Failed to remove bindmount on %s (%s)", SEC_ASECDIR_EXT, strerror(errno));
-        goto fail_remount_tmpfs;
+        if (doUnmount(Volume::SEC_ASECDIR_EXT, force)) {
+            SLOGE("Failed to remove bindmount on %s (%s)", SEC_ASECDIR_EXT, strerror(errno));
+            goto fail_remount_tmpfs;
+        }
     }
 
     /*